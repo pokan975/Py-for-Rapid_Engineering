9d28b42914fd54c5a1df9144153c736c3e24428e
pokan975 <44811052+pokan975@users.noreply.github.com>
2019-01-07 23:26:17 -0700

    Add files via upload

--- Homework_1.py
+++ Homework_1.py
@@ -1,2 +1,15 @@
 ## EEE591: Python for Rapid Engineering Solution
 ## Author: Po-Kan Shih
+
+
+## (1)
+from numpy import sqrt
+from numpy import around
+
+height = input("Enter the height of the tower in meter: ")
+time = sqrt(2*float(height)/9.81)
+
+print("The time the ball takes to hit the ground is",around(time,decimals=2),"sec")
+
+
+## (2)
