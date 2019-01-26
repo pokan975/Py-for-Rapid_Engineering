4652146a3ce94e820f611c36d80f2561fe3e0db0
pokan975 <44811052+pokan975@users.noreply.github.com>
2019-01-25 22:56:56 -0700

    homework 3 modified

--- hw3.py
+++ hw3.py
@@ -5,10 +5,27 @@
 # start amplitude: 0m~2m, with zero initial velocity
 # =============================================================================
 import numpy as np
+import comp_constants as COMP
 from read_netlist import read_netlist
+
+
+def stamper(y_add, netlist, currents, voltage, num_nodes):
+    for comp in netlist:
+        if (comp[COMP.TYPE] == COMP.R):
+            i = comp[COMP.I] - 1
+            j = comp[COMP.j] - 1
+            if (i >= 0):
+                y_add[i, i] += 1.0 / comp[COMP.VAL]
+                y_add[j, j] += 1.0 / comp[COMP.VAL]
+                y_add[i, j] += -1.0 / comp[COMP.VAL]
+                y_add[j, i] += -1.0 / comp[COMP.VAL]
+        
+        elif (comp[COMP.TYPE] == COMP.VS):
+            
+    return num_nodes
+
 
 netlist = read_netlist()
 
 N = len(netlist)
 A = np.zeros([N, N], float)
-
