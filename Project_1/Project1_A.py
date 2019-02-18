7864dda574e5c53f663249e014af3396f4c59a95
pokan975 <44811052+pokan975@users.noreply.github.com>
2019-02-17 22:25:20 -0700

    finish project 1

--- Project_1/Project1_A.py
+++ Project_1/Project1_A.py
@@ -2,15 +2,20 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib import cm
 import matplotlib.pyplot as plt
+from matplotlib import cm
+
+############################################################
+# Problem 1                                                #
+############################################################
 
 # =============================================================================
-# Problem 1
+# Function:
+# create covariance for dataframes
 # =============================================================================
 def mosthighlycorrelated(mydataframe, numtoreport): 
     
-    # find the correlations 
+    # compute the correlations 
     cormatrix = mydataframe.corr() 
     
     # set the correlations on the diagonal or lower triangle to zero, 
@@ -26,61 +31,83 @@ def mosthighlycorrelated(mydataframe, numtoreport):
     return cormatrix.head(numtoreport)
 
 
+# =============================================================================
+# Function:
+# Compute and plot the covariance matrix
+# =============================================================================
 def corr_matrix(X,cols):
-    fig = plt.figure(figsize = (7,7), dpi = 100)
+    # make the color maps for the covariance matrix
+    fig = plt.figure(figsize = (8,8), dpi = 100)
     ax1 = fig.add_subplot(111)
-    cmap = cm.get_cmap('jet', 30)
-    cax = ax1.imshow(np.abs(X.corr()), interpolation = 'nearest', cmap = cmap)
+    cmap = cm.get_cmap("jet", 30)
+    cax = ax1.imshow(np.abs(X.corr()), interpolation = "nearest", cmap = cmap)
     
+    # config colormap axis and grid
     major_ticks = np.arange(0, len(cols), 1)
     ax1.set_xticks(major_ticks)
     ax1.set_yticks(major_ticks)
-    ax1.grid(True, which = 'both', axis = 'both')
-##    plt.aspect('equal')
-    plt.title('Correlation Matrix')
+    ax1.grid(True, which = "both", axis = "both")
+
+    # config axis labels and colorbar
+    plt.title("Correlation Matrix")
     labels = cols
     ax1.set_xticklabels(labels, fontsize = 9)
     ax1.set_yticklabels(labels, fontsize = 9)
     fig.colorbar(cax, ticks = [-0.4,-0.25,-.1,0,0.1,.25,.5,.75,1])
     plt.show()
-    return(1)
 
 
+# =============================================================================
+# Function:
+# Plot pairwise relationships in heart disease dataset
+# =============================================================================
 def pairplotting(df):
-    sns.set(style = 'whitegrid', context = 'notebook')
+    # set pairplot parameters and plot the pairwise dot distribution
+    sns.set(style = "whitegrid", context = "notebook")
     cols = df.columns
     sns.pairplot(df[cols], height = 2.5)
     plt.show()
 
 # =============================================================================
 # Main Code:
-
-
+# Read the database in from heart1.csv file and analyze the data
+# Calculate correlation between every pair of the variables, show the variables
+# which are most highly correlated with each other, and pick those which are the most 
+# highly correlated with result for training classifiers
 # =============================================================================
-heart = pd.read_csv('heart1.csv')
+# read data from csv file as dataframe
+heart = pd.read_csv("heart1.csv")
 
+#extract variable names
 cols = heart.columns
 
 
-# wipe out observation data containing null value
+# drop the observations that contain null value
 for i in range(heart.values.shape[0]):
     null_value = sum(heart.iloc[i,:].isna())
     
+    # drop the row that contains null section
     if null_value != 0:
         heart.drop(heart.index[i])
         
-
-print(' Descriptive Statistics ')
+# show statistics of the data
+print("\nDescriptive Statistics:")
 print(heart.describe())
 
-print("Most Highly Correlated")
+# show the first n highly correlated variables
+print("\nMost Highly Correlated:")
 print(mosthighlycorrelated(heart, 10))
 
-print(' Covariance Matrix ')
-corr_matrix(heart.iloc[:, 0:-1], cols[0:-1])
+# show the matrix of correlation between each pair of variables
+print("\nCovariance Matrix:")
+corr_matrix(heart.iloc[:, :], cols[:])
 
-print(' Pair plotting ')
-pairplotting(heart)
+var_highlycorr = ["thal", "nmvcf", "eia", "mhr", "opst", "cpt", "a1p2"]
+heart_highlycorr = heart.loc[:, [*var_highlycorr]]
+
+# plot the pairwise distributions of all variables
+print("\nPair plotting:")
+pairplotting(heart_highlycorr)
 
 
 
--- Project_1/Project1_B.py
+++ Project_1/Project1_B.py
@@ -3,135 +3,172 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 
+############################################################
+# Problem 2                                                #
+############################################################
+
 # =============================================================================
 # Main Code:
-
-
+# Apply different classifiers on data to determine whether heart disease is present
 # =============================================================================
-heart = pd.read_csv('heart1.csv')
-
+# read data from csv file as dataframe
+heart = pd.read_csv("heart1.csv")
+#extract variable names
 cols = heart.columns
 
-# wipe out observation data containing null value
+# drop observation data that contains null value
 for i in range(heart.values.shape[0]):
     null_value = sum(heart.iloc[i,:].isnull())
     
+    # drop the row that contains null section
     if null_value != 0:
         heart.drop(heart.index[i])
         
 
-# observation data
-X = heart.loc[:,["thal", "nmvcf", "eia", "mhr"]].values
-# target
+# based on the statistical result from Project1_A.py,
+# pick variables that are the most highly correlated with the result for detection
+var_highlycorr = ["thal", "nmvcf", "eia", "mhr", "opst", "cpt"]
+
+X = heart.loc[:,[*var_highlycorr]].values
+# desired result
 Y = heart.loc[:,"a1p2"].values
 
 # split data into training set and test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 50)
 
-# apply standard normalization to data: x_std = (x - x_mean) / std
+# Preprocessing: standardly normalize data: x_std = (x - x_mean) / std
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x_train_std = sc.fit_transform(x_train)
 x_test_std = sc.fit_transform(x_test)
 
+# recombine data for later test
 x_combined = np.vstack((x_train_std, x_test_std))
 y_combined = np.hstack((y_train, y_test))
 
 total_inst = len(y_combined)
 
-print('Number in test ', total_inst)
+print("Number in test: ", total_inst)
 
 # =============================================================================
-# Perceptron
+# Perceptron classifier
 # =============================================================================
 from sklearn.linear_model import Perceptron
 
-ppn = Perceptron(max_iter = 100, tol = 1e-3, eta0 = 0.001, fit_intercept = True)
+# create classifier object and set parameters
+ppn = Perceptron(max_iter = 100, tol = 1e-3, eta0 = 0.01, fit_intercept = True, random_state = 42)
+# train classifier using training set
 ppn.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = ppn.predict(x_combined)
+# show classification accuracy
 print("\nPerceptron ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 # =============================================================================
-# Logistic Regression
+# Logistic Regression classifier
 # =============================================================================
 from sklearn.linear_model import LogisticRegression
 
-lr = LogisticRegression(C = 50.0, solver = "liblinear")
+# create classifier object and set parameters
+lr = LogisticRegression(C = 1.0, solver = "liblinear", penalty = "l2", random_state = 42)
+# train classifier using training set
 lr.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = lr.predict(x_combined)
+# show classification accuracy
 print("\nLogistic Regression ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 # =============================================================================
-# Support Vector Machine
+# Support Vector Machine classifier
+# use SVM with with linear and nonlinear kernel function, respectively
 # =============================================================================
 from sklearn.svm import SVC
 
-svm = SVC(kernel = 'rbf', gamma = 0.2, C = 2.0)
+# create linear kernel SVM classifier object and set parameters
+svm = SVC(kernel = "linear", class_weight = "balanced", C = 2.0, random_state = 42)
+# train classifier using training set
 svm.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = svm.predict(x_combined)
-print("\nSupport Vector Machine (Linear) ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+# show classification accuracy
+print("\nSupport Vector Machine (Linear kernel) ==>")
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
-from sklearn.svm import LinearSVC
-
-lsvm = LinearSVC(penalty = "l2", loss = "hinge", tol = 1e-3, C = 2.0, max_iter = 1000)
+# create nonlinear kernel SVM classifier object and set parameters
+lsvm = SVC(kernel = "rbf", gamma = 2.0, class_weight = "balanced", C = 2.0, random_state = 42)
+# train classifier using training set
 lsvm.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = lsvm.predict(x_combined)
-print("\nSupport Vector Machine (Nonlinear) ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+# show classification accuracy
+print("\nSupport Vector Machine (Nonlinear kernel) ==>")
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 # =============================================================================
-# Decision Tree
+# Decision Tree classifier
+# use normal decision tree and random forest classifier, respectively
 # =============================================================================
 from sklearn.tree import DecisionTreeClassifier
 
-tree = DecisionTreeClassifier(criterion = "entropy", max_depth = 4, random_state = 0)
+# create normal decision tree classifier object and set parameters
+tree = DecisionTreeClassifier(criterion = "entropy", max_depth = 4, random_state = 42)
+# train classifier using training set
 tree.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = tree.predict(x_combined)
+# show classification accuracy
 print("\nDecision Tree ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 from sklearn.ensemble import RandomForestClassifier
 
-forest = RandomForestClassifier(criterion = 'entropy', n_estimators = 10, random_state = 0, n_jobs = 2)
+# create random forest classifier object and set parameters
+forest = RandomForestClassifier(criterion = "gini", n_estimators = 100, class_weight = "balanced", n_jobs = -1, random_state = 42)
+# train classifier using training set
 forest.fit(x_train_std, y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = forest.predict(x_combined)
+# show classification accuracy
 print("\nRandom Forest ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 # =============================================================================
-# K Nearest Neighbor
+# K Nearest Neighbor classifier
 # =============================================================================
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors = 10, p = 2, metric = 'minkowski')
+# create classifier object and set parameters
+knn = KNeighborsClassifier(n_neighbors = 20, weights = "distance", algorithm = "auto", p = 4, metric = "minkowski")
+# train classifier using training set
 knn.fit(x_train_std,y_train)
 
+# testing classifier using the whole dataset (training + testing set)
 y_pred = knn.predict(x_combined)
+# show classification accuracy
 print("\nK Nearest Neighbor ==>")
-print('Misclassified samples: %d/%d' % ((y_combined != y_pred).sum(), total_inst))
-print('Accuracy: %.2f' % accuracy_score(y_combined, y_pred))
+print("Misclassified/total samples: %d/%d" % ((y_combined != y_pred).sum(), total_inst))
+print("Accuracy: %.2f" % accuracy_score(y_combined, y_pred))
 
 
 
